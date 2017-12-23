77d1ad5e9fd81b7ce4fd6c72d81fcdea1745b34e
Lukáš Kurčík <lukas.kurcik@gmail.com>
2017-12-23 01:13:43 +0100

    Dock more

--- MyBot.java
+++ MyBot.java
@@ -64,14 +64,40 @@ public class MyBot {
                 }
 
                 if (!hasMove) {
-                    Map<Double, Ship> nearby = gameMap.nearbyShipsByDistance(ship);
-                    for (final Ship e : nearby.values()) {
-                        if (e.getOwner() != myId) {
-                            final ThrustMove newThrustMove = Navigation.navigateShipToDock(gameMap, ship, e, Constants.MAX_SPEED);
+//                    Map<Double, Ship> nearby = gameMap.nearbyShipsByDistance(ship);
+//                    for (final Ship e : nearby.values()) {
+//                        if (e.getOwner() != myId) {
+//                            final ThrustMove newThrustMove = Navigation.navigateShipToDock(gameMap, ship, e, Constants.MAX_SPEED);
+//                            if (newThrustMove != null) {
+//                                moveList.add(newThrustMove);
+//                            }
+//                            break;
+//                        }
+//                    }
+                    for (final Entity e : gameMap.nearbyEntitiesByDistance(ship).values()) {
+                        if (e instanceof Ship && e.getOwner() != myId) {
+                            final Ship enemy = (Ship) e;
+                            final ThrustMove newThrustMove = Navigation.navigateShipToDock(gameMap, ship, enemy, Constants.MAX_SPEED);
                             if (newThrustMove != null) {
                                 moveList.add(newThrustMove);
                             }
                             break;
+                        }
+                        if (e instanceof Planet && ((Planet) e).isOwned() && e.getOwner() == myId) {
+                            Planet planet = (Planet) e;
+                            if (!planet.isFull()) {
+                                if (ship.canDock(planet)) {
+                                    moveList.add(new DockMove(ship, planet));
+                                    break;
+                                } else {
+                                    final ThrustMove newThrustMove = Navigation.navigateShipToDock(gameMap, ship, planet, Constants.MAX_SPEED);
+                                    if (newThrustMove != null) {
+                                        moveList.add(newThrustMove);
+                                        planetsOnList.add(planet);
+                                        break;
+                                    }
+                                }
+                            }
                         }
                     }
                 }
